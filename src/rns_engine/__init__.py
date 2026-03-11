728aa7b9044f912ab128f485935f38b1f8807823
Evan Wesley <ewesley541@gmail.com>
2026-03-11 14:00:41 -0400

    Update __init__.py

--- src/rns_engine/__init__.py
+++ src/rns_engine/__init__.py
@@ -1,82 +1,65 @@
-from ._core import HAS_AVX2
-from ._core import M
-from ._core import M0
-from ._core import M1
-from ._core import M2
-from ._core import M3
-from ._core import add
-from ._core import add_u64_auto
-from ._core import add_u64_io
-from ._core import add_u64_io_omp
-from ._core import affine_repeat
-from ._core import affine_repeat_u64
-from ._core import affine_repeat_u64_auto
-from ._core import affine_repeat_u64_io
-from ._core import affine_repeat_u64_io_omp
-from ._core import decode
-from ._core import div_ as div
-from ._core import encode
-from ._core import fma
-from ._core import fma_u64
-from ._core import fma_u64_auto
-from ._core import fma_u64_io
-from ._core import fma_u64_io_omp
-from ._core import mul
-from ._core import mul_u64
-from ._core import mul_u64_auto
-from ._core import mul_u64_io
-from ._core import mul_u64_io_omp
-from ._core import omp_max_threads
-from ._core import omp_num_procs
-from ._core import omp_set_num_threads
-from ._core import sub
-from ._core import sub_u64_auto
-from ._core import sub_u64_io
-from ._core import sub_u64_io_omp
-from .engine import EncodedArray
-from .engine import Session
-from .engine import SessionCache
+from ._core import (
+    HAS_AVX2,
+    M,
+    M0,
+    M1,
+    M2,
+    M3,
+    encode,
+    decode,
+    op,
+    add,
+    sub,
+    mul,
+    div_,
+    fma,
+    affine_repeat,
+    mul_u64,
+    fma_u64,
+    affine_repeat_u64,
+    add_u64_io,
+    add_u64_io_omp,
+    add_u64_auto,
+    sub_u64_io,
+    sub_u64_io_omp,
+    sub_u64_auto,
+    mul_u64_io,
+    mul_u64_io_omp,
+    mul_u64_auto,
+    fma_u64_io,
+    fma_u64_io_omp,
+    fma_u64_auto,
+    affine_repeat_u64_io,
+    affine_repeat_u64_io_omp,
+    affine_repeat_u64_auto,
+    omp_max_threads,
+    omp_set_num_threads,
+    omp_num_procs,
+)
+from .engine import EncodedArray, Session, SessionCache
 
 __version__ = "0.4.0rc1"
 
 __all__ = [
-    "__version__",
-    "M",
-    "M0",
-    "M1",
-    "M2",
-    "M3",
     "HAS_AVX2",
-    "encode",
-    "decode",
-    "add",
-    "sub",
-    "mul",
-    "div",
-    "fma",
-    "mul_u64",
-    "fma_u64",
+    "M", "M0", "M1", "M2", "M3",
+    "encode", "decode", "op",
+    "add", "sub", "mul", "div_", "fma",
     "affine_repeat",
-    "affine_repeat_u64",
-    "add_u64_io",
-    "sub_u64_io",
-    "mul_u64_io",
-    "fma_u64_io",
-    "affine_repeat_u64_io",
-    "add_u64_io_omp",
-    "sub_u64_io_omp",
-    "mul_u64_io_omp",
-    "fma_u64_io_omp",
-    "affine_repeat_u64_io_omp",
-    "add_u64_auto",
-    "sub_u64_auto",
-    "mul_u64_auto",
-    "fma_u64_auto",
-    "affine_repeat_u64_auto",
-    "omp_max_threads",
-    "omp_set_num_threads",
-    "omp_num_procs",
-    "EncodedArray",
-    "SessionCache",
-    "Session",
+    "mul_u64", "fma_u64", "affine_repeat_u64",
+    "add_u64_io", "add_u64_io_omp", "add_u64_auto",
+    "sub_u64_io", "sub_u64_io_omp", "sub_u64_auto",
+    "mul_u64_io", "mul_u64_io_omp", "mul_u64_auto",
+    "fma_u64_io", "fma_u64_io_omp", "fma_u64_auto",
+    "affine_repeat_u64_io", "affine_repeat_u64_io_omp", "affine_repeat_u64_auto",
+    "omp_max_threads", "omp_set_num_threads", "omp_num_procs",
+    "EncodedArray", "SessionCache", "Session",
+    "info",
 ]
+
+def info():
+    print(f"rns_engine v{__version__}")
+    print(f"  Dynamic range : [0, {M:,})")
+    print(f"  Moduli        : {M0} x {M1} x {M2} x {M3}")
+    print(f"  AVX2          : {'yes' if HAS_AVX2 else 'no'}")
+    print("  Core APIs     : add/sub/mul/fma + raw/omp/auto scalar-broadcast family")
